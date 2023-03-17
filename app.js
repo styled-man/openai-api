00b8f5b17460418e06fb6261f4d903fe63dfe1f7
styled <smyvensestime@gmail.com>
2023-03-17 08:17:39 -0400

    convert to es6

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require("dotenv").config(); // load the secrets
+import ENV from "dotenv"
+import express from "express";
+import { Configuration, OpenAIApi } from "openai";
 
-const express = require("express");
-const openai = require("openai");
+ENV.config() // load secrets
 
 const api = express();
 const port = 3000;
 
-const { Configuration, OpenAIApi } = openai;
-
 const config = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
-const openAIApi = new OpenAIApi(config);
+const openai = new OpenAIApi(config);
 
 // allow json to be read from the request
 api.use(express.json());
 // tell the app to render the files from the frontend folder
 api.use(express.static("frontend"));
 
+// a route that accepts post method
 api.post("/search", async (request, response) => {
-    const completion = await openAIApi.createCompletion({
+    const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: request.body.userInput,
         temperature: 0.8,
